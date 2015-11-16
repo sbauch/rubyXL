6cd24455cec26b447f4c2d6a75f75e05d6eb2d13
sbauch <sbauch@gmail.com>
2015-11-16 10:29:28 -0500

    only shift cells if row exists

--- lib/rubyXL/worksheet.rb
+++ lib/rubyXL/worksheet.rb
@@ -174,7 +174,7 @@ module LegacyWorksheet
 
     # Change cell row numbers
     row_index.upto(sheet_data.size - 1) { |index|
-      sheet_data[index].cells.each{ |c| c.row -= 1 unless c.nil? }
+      sheet_data[index].cells.each{ |c| c.row -= 1 unless c.nil? } if sheet_data[index]
     }
 
     return deleted
