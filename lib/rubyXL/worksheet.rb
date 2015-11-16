cc2e1208c2ca46704910b619465f9ad612be7178
sbauch <sbauch@gmail.com>
2015-11-16 10:31:01 -0500

    revert

--- lib/rubyXL/worksheet.rb
+++ lib/rubyXL/worksheet.rb
@@ -174,8 +174,7 @@ module LegacyWorksheet
 
     # Change cell row numbers
     row_index.upto(sheet_data.size - 1) { |index|
-      sheet_data[index].cells.each{ |c| c.row -= 1 unless c.nil? } if sheet_data[index]
-    }
+      sheet_data[index].cells.each{ |c| c.row -= 1 unless c.nil? }
 
     return deleted
   end
